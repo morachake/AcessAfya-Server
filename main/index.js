316fad7bc1fa4e736868c691ade4833706db6087
jacob-d242 <jacobmoracha@gmail.com>
2022-06-30 15:57:35 +0300

    ipdates

--- main/index.js
+++ main/index.js
@@ -22,8 +22,8 @@ app.use(
 );
 
 app.use(express.static('public'));
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
-})
+//app.get("*", (req, res) => {
+//    res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
+//})
 
 app.listen(port, console.log(`server on port ${port}`));
